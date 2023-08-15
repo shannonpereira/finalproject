7c2f5138d6587356de37591153f1f937d349aa0c
Shannon Pereira <shannonpereira971@gmail.com>
2023-08-16 00:47:06 +0530

    Second commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import './Contact.css'; // Import your stylesheet for styling
 
-const ContactUs = () => {
+const Contact = () => {
   return (
-    <center>
-    <div className="contact-card">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Contact Us</h5>
-          <p className="card-text">If you have any questions or inquiries, feel free to get in touch with us.</p>
-          <div className="contact-details">
-            <p>Email: info@healthcareclinic.com</p>
-            <p>Phone: (123) 456-7890</p>
-            <p>Address: 123 Main Street, City, Country</p>
-          </div>
+    <div className="contact-page">
+    <h2 className="contact-heading">Contact Us</h2>
+    <div className="contact-columns">
+      <div className="contact-column">
+        <img src="https://img.icons8.com/?size=1x&id=96470&format=png" alt="Phone Logo" className="logo" />
+        <h3>Phone</h3>
+        <p>Available on reference days</p>
+        <p className="subtext">Call us: <span className="phone-number">123-456-7890</span></p>
+        </div>
+        <div className="contact-column">
+        <img src="https://img.icons8.com/?size=1x&id=CmdWxCVy4gGr&format=png" alt="Case Logo" className="logo" />
+        <h3>Start a New Case</h3>
+        <p>Have a legal issue? Let's start working on it.</p>
+        <button className="action-button">Get Started</button>
+      </div>
+      <div className="contact-column">
+        <img src="https://img.icons8.com/?size=1x&id=OqS5bw76jOKP&format=png" alt="Chat Logo" className="logo" />
+        <h3>Live Chat</h3>
+        <p>Connect with us instantly through live chat.</p>
+        <button className="action-button">Chat Now</button>
         </div>
       </div>
     </div>
-    </center>
   );
 };
-
-export default ContactUs;
+export default Contact;
